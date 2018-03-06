ae43312a0c82ed20bdd75c16ab72489776847bd2
Tim Normark <timnormark@gmail.com>
2018-03-06 13:48:51 +0100

    Fixed a bug in the advanced Java-program

--- TestJava.java
+++ TestJava.java
@@ -1,6 +1,6 @@
 public class TestJava {
 	public static void main(String[] args) {
-		System.out.println("Hej allihopa, nu " + "tänker jag repetera vad du skrev");
+		System.out.println("Hej allihopa, nu tänker jag repetera vad du skrev");
 		System.out.println(args[0] +" "+ args[1]);
 	}
 }
